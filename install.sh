ccb6b352f753841ab999d88c92f6ead913663a5e
dough10 <doughten.jimi@gmail.com>
2024-08-20 23:15:11 -0500

    -e

--- install.sh
+++ install.sh
@@ -23,30 +23,30 @@ checkFolder () {
   fi 
 }
 
-echo "${PURPLE} _____    ______   _    _   ______   _    _  10${NC}";
-echo "${PURPLE}| | \ \  / |  | \ | |  | | | | ____ | |  | |   ${NC}";
-echo "${PURPLE}| | | |  | |  | | | |  | | | |  | | | |--| |   ${NC}";
-echo "${PURPLE}|_|_/_/  \_|__|_/ \_|__|_| |_|__|_| |_|  |_|   ${NC}";
-echo "${WHITE}Openauto Script Installer v${NC}${GREEN}${version}${NC}";
-echo "${PURPLE}                                               ${NC}";
+echo -e "${PURPLE} _____    ______   _    _   ______   _    _  10${NC}";
+echo -e "${PURPLE}| | \ \  / |  | \ | |  | | | | ____ | |  | |   ${NC}";
+echo -e "${PURPLE}| | | |  | |  | | | |  | | | |  | | | |--| |   ${NC}";
+echo -e "${PURPLE}|_|_/_/  \_|__|_/ \_|__|_| |_|__|_| |_|  |_|   ${NC}";
+echo -e "${WHITE}Openauto Script Installer v${NC}${GREEN}${version}${NC}";
+echo -e "${PURPLE}                                               ${NC}";
 
-echo "${YELLOW}Updating app cache${NC}"
+echo -e "${YELLOW}Updating app cache${NC}"
 sudo apt-get update
 
-echo "${YELLOW}Installing Requirments${NC}"
+echo -e "${YELLOW}Installing Requirments${NC}"
 sudo apt install xterm python3 python3-pip -y
 
-echo "${YELLOW}Installing pynput${NC}"
+echo -e "${YELLOW}Installing pynput${NC}"
 python3 -m pip install pynput
 
-echo "${YELLOW}Checking for autostart folder${NC}"
+echo -e "${YELLOW}Checking for autostart folder${NC}"
 checkFolder "/home/pi/.config/autostart"
 
-echo "${YELLOW}Installing / Updating Python scripts${NC}"
+echo -e "${YELLOW}Installing / Updating Python scripts${NC}"
 cp -u -p -v -r classes /home/pi
 cp -u -p -v power.py /home/pi
 cp -u -p -v vol.py /home/pi
 
-echo "${YELLOW}Installing / Updating autostart shortcuts${NC}"
+echo -e "${YELLOW}Installing / Updating autostart shortcuts${NC}"
 cp -u -p -v power.desktop /home/pi/.config/autostart
 cp -u -p -v vol.desktop /home/pi/.config/autostart
