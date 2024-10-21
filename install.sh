67405dfe0b1dc7b1698c3a931091d5d3d9148e50
dough10 <doughten.jimi@gmail.com>
2024-10-21 10:26:47 -0500

    wildcard

--- install.sh
+++ install.sh
@@ -57,8 +57,7 @@ echo -e "${YELLOW}Creating autostart folder${NC}"
 createFolder "~/.config/autostart"
 
 echo -e "${YELLOW}Installing autostart shortcuts${NC}"
-cp -u -p -v ~/openauto-scripts/power.desktop ~/.config/autostart
-cp -u -p -v ~/openauto-scripts/vol.desktop ~/.config/autostart
+cp -u -p -v ~/openauto-scripts/*.desktop ~/.config/autostart
 
 chmod +x ~/.config/autostart/vol.desktop
 chmod +x ~/.config/autostart/power.desktop
