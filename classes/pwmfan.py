4ca202e6cd8b121600d0129eb0569bc7ac854b49
dough10 <doughten.jimi@gmail.com>
2024-10-22 08:30:50 -0500

    curvy

--- classes/pwmfan.py
+++ classes/pwmfan.py
@@ -23,23 +23,26 @@ class Pwnfan:
   def main(self) -> None:
     result = subprocess.run(['/usr/bin/vcgencmd', 'measure_temp'], capture_output=True, text=True)
     reading = float(result.stdout.split('=')[1].split("'")[0])
-    logger.info(reading)
+    logger.info(f"Temperature reading: {reading}")
+
     duty_cycles = [
-        (75.0, 100.0),
-        (70.0, 70.0),
-        (57.0, 55.0),
-        (55.0, 53.0),
-        (34.0, 47.0),
-        (32.0, 45.0),
-        (30.0, 30.0)
+      (75.0, 100.0),
+      (70.0, 85.0),
+      (65.0, 70.0),
+      (60.0, 55.0),
+      (50.0, 45.0),
+      (40.0, 35.0),
+      (30.0, 30.0)
     ]
 
     for threshold, duty in duty_cycles:
-        if reading >= threshold:
-            self.__fan.ChangeDutyCycle(duty)
-            return
+      if reading >= threshold:
+        self.__fan.ChangeDutyCycle(duty)
+        logger.info(f"Fan speed adjusted to {duty}% for temperature {reading}°C")
+        return
 
     self.__fan.ChangeDutyCycle(20.0)
+    logger.info("Fan speed set to 20% (default)")
       
 
 if __name__ == "__main__":
