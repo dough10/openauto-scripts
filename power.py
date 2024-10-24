53072fc92a996698a720c5cfb8ef86d727a1ff39
dough10 <doughten.jimi@gmail.com>
2024-10-24 09:14:00 -0500

    latch test

--- power.py
+++ power.py
@@ -23,15 +23,18 @@ class Ignition:
   __IGN_LOW_TIME:int = 3
   __ignLowCounter:int = 0
 
-  def __init__(self, ign_pin:int, remote_pin:int, fan_pin:int) -> None:
+  def __init__(self, ign_pin:int, remote_pin:int, fan_pin:int, latch_pin:int) -> None:
     logger.info('Starting power.py')
     logger.info(f'ignotion pin:{ign_pin}, remote pin:{remote_pin}, fan pin:{fan_pin}')
     self.__pin = ign_pin
+    self.__latch_pin = latch_pin
     self.__remote = Remote(remote_pin)
     self.__fan = Pwnfan(fan_pin)
     GPIO.setup(self.__pin, GPIO.IN)
+    GPIO.setup(self.__latch_pin, GPIO.OUT)
     time.sleep(5)
     self.__remote.on()
+    GPIO.output(self.__latch_pin, 1)
 
   def __keypress(self, key:str) -> None:
     keycontroller.press(key)
@@ -57,8 +60,9 @@ if __name__ == "__main__":
   IGN_PIN = 17
   REMOTE_PIN = 25
   FAN_PIN = 12
+  LATCH_PIN = 4
 
-  ignition = Ignition(IGN_PIN, REMOTE_PIN, FAN_PIN)
+  ignition = Ignition(IGN_PIN, REMOTE_PIN, FAN_PIN, LATCH_PIN)
 
   try:
     while True:
