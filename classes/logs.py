01c3468048bfe487ff6a0535b6bca9e833e7dc5e
dough10 <doughten.jimi@gmail.com>
2024-10-24 08:19:14 -0500

    clear logs on startup

--- classes/logs.py
+++ classes/logs.py
@@ -3,11 +3,11 @@ import logging.config
 
 class Logs:
   def __init__(self):
-    self.__logger = logging.getLogger('')
+    self.__logger = logging.getLogger('openauto-scripts')
     self.__logger.setLevel(logging.INFO)
 
     if not self.__logger.handlers:
-      file_handler = logging.FileHandler('info.log')
+      file_handler = logging.FileHandler('info.log', mode='w')
       formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
       file_handler.setFormatter(formatter)
       self.__logger.addHandler(file_handler)
