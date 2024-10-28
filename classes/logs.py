72b011e7d87b84086874c1c9e4f58d2d4ce55baf
dough10 <doughten.jimi@gmail.com>
2024-10-28 15:53:47 -0500

    better logging?

--- classes/logs.py
+++ classes/logs.py
@@ -1,16 +1,22 @@
+import os
 import logging
 import logging.config
+from logging.handlers import RotatingFileHandler
 
 class Logs:
-  def __init__(self):
+  def __init__(self, filename: str, level: int = logging.INFO, max_bytes: int = 5_000_000, backup_count: int = 5):
     self.__logger = logging.getLogger('openauto-scripts')
-    self.__logger.setLevel(logging.INFO)
+    self.__logger.setLevel(level)
 
     if not self.__logger.handlers:
-      file_handler = logging.FileHandler('info.log');
+      file_handler = RotatingFileHandler(f'{os.path.basename(filename)}.log', maxBytes=max_bytes, backupCount=backup_count)
       formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
       file_handler.setFormatter(formatter)
       self.__logger.addHandler(file_handler)
+
+      stream_handler = logging.StreamHandler()
+      stream_handler.setFormatter(formatter)
+      self.__logger.addHandler(stream_handler)
 
   def get_logger(self):
     return self.__logger
