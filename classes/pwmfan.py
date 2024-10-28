d8b7888633d0870a21dd70a2c2ca7b08fa1a31a1
dough10 <doughten.jimi@gmail.com>
2024-10-28 16:25:39 -0500

    ads

--- classes/pwmfan.py
+++ classes/pwmfan.py
@@ -17,10 +17,20 @@ GPIO.setwarnings(False)
 logger = Logs(__file__).get_logger()
 
 class Pwnfan:
-  def __init__(self, fan_pin:int, duty_cycles:List[Tuple[float, float]], default_duty: float = 20.0) -> None:
+  def __init__(self, fan_pin:int) -> None:
     logger.info(f'Starting {__file__}')
-    self.__duty_cycles = duty_cycles
-    self.__default_duty = default_duty
+    # (temperature threshold, fan speed precentage)
+    self.__duty_cycles:List[Tuple[float, float]] = [
+      (75.0, 100.0),
+      (70.0, 85.0),
+      (65.0, 70.0),
+      (60.0, 55.0),
+      (50.0, 45.0),
+      (40.0, 35.0),
+      (30.0, 30.0),
+      (20.0, 0.0)
+    ]
+    self.__default_duty: float = 20.0
     self.__pin = fan_pin
     GPIO.setup(self.__pin, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
     self.__fan = GPIO.PWM(self.__pin, 1000)
@@ -52,18 +62,8 @@ class Pwnfan:
 if __name__ == "__main__":
   # GPIO pin number
   pin = 12
-  # (temperature threshold, fan speed precentage)
-  duty_cycles = [
-    (75.0, 100.0),
-    (70.0, 85.0),
-    (65.0, 70.0),
-    (60.0, 55.0),
-    (50.0, 45.0),
-    (40.0, 35.0),
-    (30.0, 30.0),
-    (20.0, 0.0)
-  ]
-  fan = Pwnfan(pin, duty_cycles)
+  
+  fan = Pwnfan(pin)
   
   try:
     while True:
