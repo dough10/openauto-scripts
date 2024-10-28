457226ea39797fa36dc3262ae681eeb7dacd78df
dough10 <doughten.jimi@gmail.com>
2024-10-28 06:41:28 -0500

    pass in duty_cycles

--- classes/pwmfan.py
+++ classes/pwmfan.py
@@ -16,7 +16,8 @@ GPIO.setwarnings(False)
 logger = Logs().get_logger()
 
 class Pwnfan:
-  def __init__(self, fan_pin:int) -> None:
+  def __init__(self, fan_pin:int, duty_cycles) -> None:
+    self.__duty_cycles = duty_cycles
     self.__pin = fan_pin
     GPIO.setup(self.__pin, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
     self.__fan = GPIO.PWM(self.__pin, 1000)
@@ -27,17 +28,7 @@ class Pwnfan:
     result = subprocess.run(['/usr/bin/vcgencmd', 'measure_temp'], capture_output=True, text=True)
     reading = float(result.stdout.split('=')[1].split("'")[0])
 
-    duty_cycles = [
-      (75.0, 100.0),
-      (70.0, 85.0),
-      (65.0, 70.0),
-      (60.0, 55.0),
-      (50.0, 45.0),
-      (40.0, 35.0),
-      (30.0, 30.0)
-    ]
-
-    for threshold, duty in duty_cycles:
+    for threshold, duty in self.__duty_cycles:
       if reading >= threshold:
         self.__fan.ChangeDutyCycle(duty)
         logger.info(f"Fan speed adjusted to {duty}% for temperature {reading}°C")
@@ -48,6 +39,19 @@ class Pwnfan:
       
 
 if __name__ == "__main__":
-  fan = Pwnfan(12)
+  '''
+  [
+    (temperature threshold, fan speed precentage)
+  ]
+  '''
+  fan = Pwnfan(12, [
+    (75.0, 100.0),
+    (70.0, 85.0),
+    (65.0, 70.0),
+    (60.0, 55.0),
+    (50.0, 45.0),
+    (40.0, 35.0),
+    (30.0, 30.0)
+  ])
   while True:
     fan.main()  
