513a08da225118c715e5548e69ec1264ec3215be
dough10 <doughten.jimi@gmail.com>
2024-10-30 00:17:35 -0500

    sd

--- classes/pwmfan.py
+++ classes/pwmfan.py
@@ -37,10 +37,10 @@ class Pwnfan:
   
   def __init__(self, fan_pin:int, speed_pin:int) -> None:
     logger.info(f'Starting {__file__}')
-    GPIO.setup(fan_pin, GPIO.OUT)
-    self.__fan = GPIO.PWM(fan_pin, self.__frequency)
     GPIO.setup(speed_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(speed_pin, GPIO.FALLING, self.__fell)
+    GPIO.setup(fan_pin, GPIO.OUT)
+    self.__fan = GPIO.PWM(fan_pin, self.__frequency)
     self.__fan.start(0)
     
   @debounce(30)
