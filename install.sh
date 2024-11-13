f017db3025bae88816d1e37f5a7cefcca9cdd94f
dough10 <doughten.jimi@gmail.com>
2024-11-13 17:33:27 -0600

    words

--- install.sh
+++ install.sh
@@ -49,9 +49,9 @@ echo -e "${YELLOW}Setup virtual environment${NC}"
 python3 -m venv ~/openauto-scripts/.venv
 echo -e "${YELLOW}virtual environment ~/openauto-scripts/.venv created${NC}"
 
-echo -e "${YELLOW}Installing dependencies${NC}"
+echo -e "${YELLOW}Installing requirments.txt${NC}"
 ~/openauto-scripts/.venv/bin/python3 -m pip install -r ~/openauto-scripts/requirments.txt
-echo -e "${YELLOW}dependencies installed${NC}"
+echo -e "${YELLOW}requirments.txt installed${NC}"
 
 echo -e "${YELLOW}Creating autostart folder${NC}"
 createFolder "~/.config/autostart"
