83ac12379750a6da73de24ffe0e436412b6ad86e
dough10 <doughten.jimi@gmail.com>
2024-11-13 19:35:34 -0600

    env log_level

--- classes/logs.py
+++ classes/logs.py
@@ -3,10 +3,17 @@ import logging
 import logging.config
 from logging.handlers import RotatingFileHandler
 
+log_level_str = os.getenv('LOG_LEVEL', 'WARNING').upper()
+
+try:
+  log_level = getattr(logging, log_level_str)
+except AttributeError:
+  raise ValueError(f"Invalid log level: {log_level_str}. Must be one of: DEBUG, INFO, WARNING, ERROR, CRITICAL.")
+
 class Logs:
-  def __init__(self, filename: str, level: int = logging.INFO, max_bytes: int = 5_000_000, backup_count: int = 5):
+  def __init__(self, filename: str, max_bytes: int = 5_000_000, backup_count: int = 5):
     self.__logger = logging.getLogger('openauto-scripts')
-    self.__logger.setLevel(level)
+    self.__logger.setLevel(level=log_level)
 
     if not self.__logger.handlers:
       file_handler = RotatingFileHandler(f'{os.path.basename(filename)}.log', maxBytes=max_bytes, backupCount=backup_count)
