109e0cb49c55be104ca8f82afaaf5e2e7c7261db
dough10 <doughten.jimi@gmail.com>
2024-11-21 22:47:50 -0600

    script to remove all recordings

---
+++ remove_recordings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ENV_FILE=".env"
+
+if [[ -f "$ENV_FILE" ]]; then
+  echo ".env file found, sourcing variables..."
+  source "$ENV_FILE"
+else
+  echo ".env file not found, using default values..."
+fi
+
+LOC=${REC_LOC:-"$HOME/Videos"}
+
+if [[ -d "$LOC" ]]; then
+  echo "removing recording from: $LOC/*.h264"
+  rm -v $LOC/*.h264
+else
+  echo "Directory $LOC does not exist."
+fi
