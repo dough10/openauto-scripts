639df71532d7e615ee7f6028fe1aee89adb15466
dough10 <doughten.jimi@gmail.com>
2024-11-21 20:38:46 -0600

    all env passed into class, add rec_loc

--- power.py
+++ power.py
@@ -33,11 +33,11 @@ class Ignition:
     __pin (int): GPIO pin used to monitor ignition state (e.g., car ignition signal).
     __remote (Remote): Remote control instance for handling remote power state.
     __fan (Pwmfan): PWM fan instance for controlling fan state.
+    __dashcam (Dashcam): Dashcam object for recording dashcam-style footage
   """
-  __IGN_LOW_TIME:int = int(os.getenv('IGN_LOW_TIME', 3))
   __ignLowCounter:int = 0
 
-  def __init__(self, ign_pin:int, remote_pin:int, fan_pin:int, fan_speed_pin:int, latch_pin:int) -> None:
+  def __init__(self, ign_pin:int, remote_pin:int, fan_pin:int, fan_speed_pin:int, latch_pin:int, ign_low_time:int, rec_loc:str = '') -> None:
     """
     Initializes the Ignition object and sets up GPIO pins and devices.
     
@@ -47,8 +47,11 @@ class Ignition:
       fan_pin (int): GPIO pin connected to the fan.
       fan_speed_pin (int): GPIO pin connected to the fan speed tachometer.
       latch_pin (int): GPIO pin used for the latch power control.
+      ign_low_time (int): time to wait before shutdown code is ran
+      rec_loc (str):  The directory where the video file will be saved.
     """
     logger.info(f'Starting {__file__}, IGN_PIN:{ign_pin}, LATCH_PIN:{latch_pin}')
+    self.__IGN_LOW_TIME = ign_low_time
     self.__pin = ign_pin
     self.__remote = Remote(remote_pin)
     self.__fan = Pwmfan(fan_pin, fan_speed_pin)
@@ -56,7 +59,7 @@ class Ignition:
     self.__latch_power(latch_pin)
     time.sleep(3) # give time for system to complete boot before turning on remote
     self.__remote.on()
-    self.__dashcam = Dashcam()
+    self.__dashcam = Dashcam(rec_loc)
 
   def __keypress(self, key:str) -> None:
     """
@@ -123,13 +126,15 @@ if __name__ == "__main__":
   REMOTE_PIN = int(os.getenv('REMOTE_PIN', 25))
   FAN_PIN = int(os.getenv('FAN_PIN', 12))
   FAN_SPEED_PIN = int(os.getenv('FAN_SPEED_PIN', 24))
+  IGN_LOW_TIME = int(os.getenv('IGN_LOW_TIME', 3))
+  REC_LOC = os.getenv('REC_LOC', '$HOME/Videos')
 
-  ignition = Ignition(IGN_PIN, REMOTE_PIN, FAN_PIN, FAN_SPEED_PIN, LATCH_PIN)
+  ignition = Ignition(IGN_PIN, REMOTE_PIN, FAN_PIN, FAN_SPEED_PIN, LATCH_PIN, IGN_LOW_TIME, REC_LOC)
 
   try:
     while True:
       ignition.main()
-      time.sleep(2)
+      time.sleep(1)
   except Exception as e:
     logger.exception("main crashed. Error: %s", e) 
     exit()
