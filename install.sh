2eff6b93aef08cb8bc3deef5d877ba1d98d5bee6
dough10 <doughten.jimi@gmail.com>
2024-11-23 17:34:15 -0600

    i can't do 1 file dl / install if i need 2 files

--- install.sh
+++ install.sh
@@ -2,7 +2,17 @@
 set -e
 version="1.4"
 
-source ./sh/colors.sh
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+GREY='\033[0;37m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+NC='\033[0m'
 
 createFolder () {
   folderPath="${1/#\~/$HOME}"
