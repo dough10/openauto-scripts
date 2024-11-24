f6e1eeb0802ce2d3e7853cce4042d855e5d109da
dough10 <doughten.jimi@gmail.com>
2024-11-24 03:27:29 -0600

    option to disable more features

--- oas.py
+++ oas.py
@@ -8,11 +8,11 @@ from classes.dashcam import Dashcam
 from classes.ignition import Ignition
 from classes.vol import Volume
 
+load_dotenv()
+
 def feature_is_enabled(feature:str) -> bool:
   env:str = os.getenv(feature, 'false')
   return env.lower() in ['true', '1', 't', 'y', 'yes']
-
-load_dotenv()
 
 IGN_PIN = int(os.getenv('IGN_PIN', 17))
 LATCH_PIN = int(os.getenv('LATCH_PIN', 4))
@@ -22,18 +22,20 @@ FAN_SPEED_PIN = int(os.getenv('FAN_SPEED_PIN', 24))
 IGN_LOW_TIME = int(os.getenv('IGN_LOW_TIME', 3))
 REC_LOC = os.getenv('REC_LOC', '~/Videos')
 
-fan = Pwmfan(FAN_PIN, FAN_SPEED_PIN)
 rem = Remote(REMOTE_PIN)
-vol = Volume()
+if feature_is_enabled('PWM'): 
+  fan = Pwmfan(FAN_PIN, FAN_SPEED_PIN)
+if feature_is_enabled('VOL'): 
+  vol = Volume()
 if feature_is_enabled('DASHCAM'): 
   cam = Dashcam(location=REC_LOC)
 
 def shutdown() -> None:
-  vol.stop_listener()
+  if vol: vol.stop_listener()
   rem.off()
   if cam: cam.stop()
   rem.cleanup()
-  fan.cleanup()
+  if fan: fan.cleanup()
     
 ign = Ignition(IGN_PIN, LATCH_PIN, IGN_LOW_TIME, shutdown)
 
