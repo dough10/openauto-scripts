08a2dd8b2d937e8af9b7995d676677bd85c16741
dough10 <doughten.jimi@gmail.com>
2024-11-27 15:48:32 -0600

    allow cap

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,6 @@ rm ~/openauto-scripts.log
 sudo rm /usr/local/bin/purge_recordings
 echo "remove ~/fan_curves folder? (y,n)"
 read -r response
-if [ "$response" == "y" ]; then
+if [ "$response" == "y" ] || [ "$response" == "Y" ]; then
   rm -rf ~/fan_curves
 fi
