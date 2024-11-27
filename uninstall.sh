f0037e1cc01f71364e0c86ade7733aca81b5a628
dough10 <doughten.jimi@gmail.com>
2024-11-27 15:57:07 -0600

    -f

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 rm -rf ~/.openauto-scripts
-rm ~/.config/autostart/oas.desktop
-rm ~/openauto-scripts.log
-sudo rm /usr/local/bin/purge_recordings
+rm -f ~/.config/autostart/oas.desktop
+rm -f ~/openauto-scripts.log
+sudo rm -f /usr/local/bin/purge_recordings
 echo "remove ~/fan_curves folder? (y,n)"
 read -r response
 if [ "$response" == "y" ] || [ "$response" == "Y" ]; then
