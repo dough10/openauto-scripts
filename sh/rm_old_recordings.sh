f5883db8d8cc27de22ba1c89c843d7f7345573e2
dough10 <doughten.jimi@gmail.com>
2024-11-28 00:01:02 -0600

    logs?

--- sh/rm_old_recordings.sh
+++ sh/rm_old_recordings.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+LOG_FILE=~/rm.log
+
 ENV_FILE=~/.openauto-scripts/.env
 
 if [[ -f "$ENV_FILE" ]]; then
-  echo ".env file found, sourcing variables..."
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - .env file found, sourcing variables..." >> "$LOG_FILE"
   source "$ENV_FILE"
 else
-  echo ".env file not found, using default values..."
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - .env file not found, using default values..." >> "$LOG_FILE"
 fi
 
 LOC=${REC_LOC:-"$HOME/Videos"}
 
 if [[ -d "$LOC" ]]; then
-  echo "Removing .h264 files older than 2 weeks from: $LOC"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Removing .h264 files older than 2 weeks from: $LOC" >> "$LOG_FILE"
   find "$LOC" -name "*.h264" -mtime +14 -exec rm -v {} \;
 else
-  echo "Directory $LOC does not exist."
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Directory $LOC does not exist." >> "$LOG_FILE"
 fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - old recording removed" >> "$LOG_FILE" 2>&1
