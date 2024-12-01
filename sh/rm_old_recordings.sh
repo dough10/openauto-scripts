2645328edc3f8c94e86c1bd487954123bce6befb
dough10 <doughten.jimi@gmail.com>
2024-11-30 20:20:11 -0600

    change logging

--- sh/rm_old_recordings.sh
+++ sh/rm_old_recordings.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-LOG_FILE=~/.openauto-scripts/rm.log
-
 ENV_FILE=~/.openauto-scripts/.env
 
 if [[ -f "$ENV_FILE" ]]; then
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - .env file found, sourcing variables..." >> "$LOG_FILE"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - .env file found, sourcing variables..."
   source "$ENV_FILE"
 else
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - .env file not found, using default values..." >> "$LOG_FILE"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - .env file not found, using default values..."
 fi
 
 LOC=${REC_LOC:-"$HOME/Videos"}
 
 if [[ -d "$LOC" ]]; then
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Removing .h264 files older than 2 weeks from: $LOC" >> "$LOG_FILE"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Removing .h264 files older than 2 weeks from: $LOC"
   find "$LOC" -name "*.h264" -mtime +14 -exec rm -v {} \;
 else
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Directory $LOC does not exist." >> "$LOG_FILE"
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Directory $LOC does not exist."
 fi
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - old recording removed" >> "$LOG_FILE" 2>&1
+echo "$(date '+%Y-%m-%d %H:%M:%S') - old recording removed"
