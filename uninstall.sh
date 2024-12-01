a4266d8e83facfba4091769b185d7224b8b3ff52
dough10 <doughten.jimi@gmail.com>
2024-12-01 11:48:47 -0600

    rm_old

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,7 @@
 set -e
 rm -rf ~/.openauto-scripts
 rm -f ~/.config/autostart/oas.desktop
+rm -f ~/.config/autostart/rm_old.desktop
 rm -f ~/openauto-scripts.log
 sudo rm -f /usr/local/bin/purge_recordings
 echo "remove ~/fan_curves folder? (y,n)"
