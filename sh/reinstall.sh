ef699079052c5f657b682f073541bdc2f930f255
dough10 <doughten.jimi@gmail.com>
2024-12-03 14:16:37 -0600

    wad

--- sh/reinstall.sh
+++ sh/reinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp -vn ~/.openauto-scripts/.env ~/.oas-env
+[ -f ~/.openauto-scripts/.env ] && cp -v ~/.openauto-scripts/.env ~/.oas-env
 bash ~/.openauto-scripts/uninstall.sh
 cd ~/
 curl -O https://raw.githubusercontent.com/dough10/openauto-scripts/refs/heads/main/install.sh
 bash install.sh
 rm install.sh
-cp -vn ~/.oas-env ~/.openauto-scripts/.env
+[ -f ~/.oas-env ] && cp -vn ~/.oas-env ~/.openauto-scripts/.env
