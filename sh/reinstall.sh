02c445531234d6b2b2e0806eb371203ea23cb6d5
dough10 <doughten.jimi@gmail.com>
2024-12-03 13:45:54 -0600

    backup confg when reinstalling

--- sh/reinstall.sh
+++ sh/reinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cp -v ~/.openauto-scripts/.env ~/.oas-env
 bash ~/.openauto-scripts/uninstall.sh
 cd ~/
 curl -O https://raw.githubusercontent.com/dough10/openauto-scripts/refs/heads/main/install.sh
 bash install.sh
 rm install.sh
+cp -v ~/.oas-env ~/.openauto-scripts/.env
