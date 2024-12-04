2a6e1e881c4ba0b12ccb3a4f4ec857130ae1d2dc
dough10 <doughten.jimi@gmail.com>
2024-12-04 03:51:17 -0600

    more dashcam settings

--- oas.py
+++ oas.py
@@ -15,15 +15,24 @@ logger = Logs().get_logger()
 
 load_dotenv()
 
+# oas.py
 START_DELAY = int(os.getenv('START_DELAY', 3))
+# ignition.py
 IGN_PIN = int(os.getenv('IGN_PIN', 17))
 LATCH_PIN = int(os.getenv('LATCH_PIN', 4))
 IGN_LOW_TIME = int(os.getenv('IGN_LOW_TIME', 3))
+# remote.py
 REMOTE_PIN = int(os.getenv('REMOTE_PIN', 25))
+# pwm.py
 FAN_PIN = int(os.getenv('FAN_PIN', 12))
 FAN_SPEED_PIN = int(os.getenv('FAN_SPEED_PIN', 24))
 CUSTOM_CURVE = os.getenv('CUSTOM_CURVE', None)
+# dashcam.py
 REC_LOC = os.getenv('REC_LOC', '~/Videos')
+REC_WIDTH = int(os.getenv('REC_WIDTH', 1280))
+REC_HEIGHT = int(os.getenv('REC_HEIGHT', 720))
+REC_BITRATE = int(os.getenv('REC_BITRATE', 5))
+REC_FPS = int(os.getenv('REC_FPS', 30))
 
 def feature_is_enabled(feature:str) -> bool:
   env:str = os.getenv(feature, 'false')
@@ -41,7 +50,7 @@ if feature_is_enabled('PWM'):
 if feature_is_enabled('VOL'): 
   vol = Volume()
 if feature_is_enabled('DASHCAM'): 
-  cam = Dashcam(location=REC_LOC)
+  cam = Dashcam(REC_LOC, REC_WIDTH, REC_HEIGHT, REC_BITRATE, REC_FPS)
 
 def shutdown() -> None:
   if 'vol' in globals():
